95ec5e9cedc087ae0af3ce6889af77119cfc7fba
habibuoy <habibiemuhammad90@gmail.com>
2025-05-20 14:58:21 +0700

    Add VoteSubjectDto and VoteCountDto

---
+++ sr-server/Models/Dtos/VoteCountDto.cs
@@ -0,0 +1,7 @@
+namespace SignalRDemo.Server.Models.Dtos;
+
+public class VoteCountDto
+{
+    public required string Id { get; set; }
+    public required int Count { get; set; }
+}
---
+++ sr-server/Models/Dtos/VoteSubjectDto.cs
@@ -0,0 +1,7 @@
+namespace SignalRDemo.Server.Models.Dtos;
+
+public class VoteSubjectDto
+{
+    public required string Id { get; set; }
+    public required string Name { get; set; }
+}
