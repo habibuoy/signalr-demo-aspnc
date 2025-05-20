0402e52ab5176b5179894355612476acceaf48f8
habibuoy <habibiemuhammad90@gmail.com>
2025-05-20 14:59:51 +0700

    Update VoteCountDto and VoteSubjectDto Id data type

--- sr-server/Models/Dtos/VoteCountDto.cs
+++ sr-server/Models/Dtos/VoteCountDto.cs
@@ -2,6 +2,6 @@ namespace SignalRDemo.Server.Models.Dtos;
 
 public class VoteCountDto
 {
-    public required string Id { get; set; }
+    public required int Id { get; set; }
     public required int Count { get; set; }
 }
--- sr-server/Models/Dtos/VoteSubjectDto.cs
+++ sr-server/Models/Dtos/VoteSubjectDto.cs
@@ -2,6 +2,6 @@ namespace SignalRDemo.Server.Models.Dtos;
 
 public class VoteSubjectDto
 {
-    public required string Id { get; set; }
+    public required int Id { get; set; }
     public required string Name { get; set; }
 }
