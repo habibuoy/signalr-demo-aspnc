c5f9f77498c8fcce2efb0d4d250b397b0fd9926c
habibuoy <habibiemuhammad90@gmail.com>
2025-05-21 13:13:20 +0700

    Add InputPrompter in Shared

---
+++ Shared/InputPrompter.cs
@@ -0,0 +1,44 @@
+namespace SignalRDemo.Shared;
+
+public class InputPrompter
+{
+    private readonly string? info;
+    private readonly string prompt;
+    private readonly string[]? acceptableInputs;
+    private readonly Func<string, bool>? inputEvaluator;
+
+    public InputPrompter(string prompt, string? info = null,
+        string[]? acceptableInputs = null)
+    {
+        this.info = info;
+        this.prompt = prompt;
+        this.acceptableInputs = acceptableInputs;
+    }
+
+    public InputPrompter(string prompt, string? info = null,
+        Func<string, bool>? inputEvaluator = null)
+    {
+        this.info = info;
+        this.prompt = prompt;
+        this.inputEvaluator = inputEvaluator;
+    }
+
+    public string Prompt()
+    {
+        if (!string.IsNullOrEmpty(info))
+        {
+            Console.WriteLine(info);
+        }
+
+        string? input = string.Empty;
+        do
+        {
+            Console.Write(prompt);
+            input = Console.ReadLine();
+        }
+        while ((acceptableInputs != null && !acceptableInputs.Contains(input))
+            || (inputEvaluator != null && !inputEvaluator(input!)));
+
+        return input!;
+    }
+}
