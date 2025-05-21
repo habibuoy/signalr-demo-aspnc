028aa1c4e453673a1d9d1602ee8de006a79e7e86
habibuoy <habibiemuhammad90@gmail.com>
2025-05-21 12:13:50 +0700

    Update IVoteService method summary

--- sr-server/Interfaces/IVoteService.cs
+++ sr-server/Interfaces/IVoteService.cs
@@ -1,4 +1,5 @@
 using SignalRDemo.Server.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace SignalRDemo.Server.Interfaces;
 
@@ -7,5 +8,10 @@ public interface IVoteService
     Task<Vote?> GetVoteByIdAsync(string id);
     Task<bool> AddVoteAsync(Vote vote);
     Task<bool> RemoveVoteAsync(string id);
+    /// <summary>
+    /// Update the vote asynchronously
+    /// </summary>
+    /// <returns>A boolean to indicate whether the process is success or not</returns>
+    /// <exception cref="DbUpdateConcurrencyException"></exception>
     Task<bool> UpdateVoteAsync(string voteId, Vote vote);
 }
