3a8a99dfb42f243596714a9c50f29f44dc2871b9
habibuoy <habibiemuhammad90@gmail.com>
2025-05-26 15:48:48 +0700

    Update flooder program to handler the register and login

--- Flooder/Program.cs
+++ Flooder/Program.cs
@@ -1,5 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System.Diagnostics;
+using System.Net;
+using System.Net.Http.Headers;
 using System.Net.Http.Json;
 using System.Text.Json;
 using System.Text.Json.Nodes;
@@ -13,6 +15,14 @@ var serviceProvider = services.BuildServiceProvider();
 
 const float DelayBeforePostingVote = 1f; // in s
 const float DelayBeforeFlood = 3f; // in s
+
+const string LoginUrl = "https://localhost:7000/login";
+const string RegisterUrl = "https://localhost:7000/register";
+const string BaseVoteUrl = "https://localhost:7000/vote";
+const string CreateVoteUrl = $"{BaseVoteUrl}/create";
+const string Email = "creator@gmail.com";
+const string Password = "Password123@";
+const string ServerResponseHeaderCookieKey = "Set-Cookie";
 
 var floodTypePrompter = new InputPrompter("Choose (1/2):",
     "+++++++++++\n" +
@@ -44,16 +54,35 @@ while (true)
 
         await Task.Delay((int)(DelayBeforePostingVote * 1000));
 
-        voteId = await PostVoteAsync(null);
+        voteId = await PostVoteAsync(httpClientFactory.CreateClient(), null, 60 * 5);
     }
     else
     {
         var httpClient = httpClientFactory.CreateClient();
-        var result = await httpClient.GetAsync($"https://localhost:7000/vote/{voteId}");
-        if (!result.IsSuccessStatusCode)
+
+        try
         {
-            Console.WriteLine("There is no vote with provided Vote id");
-            voteId = null;
+            var loginResponse = await LoginCreatorAsync(httpClient);
+            if (loginResponse.cookie == null)
+            {
+                Console.WriteLine($"Failed when checking vote: {loginResponse.message}");
+                voteId = null;
+            }
+            else
+            {
+                var result = await SendHttpRequestAsync(httpClient,
+                    $"{BaseVoteUrl}/{voteId}", HttpMethod.Get, cookie: loginResponse.cookie);
+                if (!result.success)
+                {
+                    Console.WriteLine($"Failed when checking vote: {result.message}");
+                    voteId = null;
+                }
+            }
+
+        }
+        finally
+        {
+            httpClient.Dispose();
         }
     }
 
@@ -119,26 +148,50 @@ while (true)
 static async Task FloodTaskAsync(int index, string voteId,
     IHttpClientFactory? httpClientFactory = null, int voteMaxRetry = 1)
 {
-    var httpClient = httpClientFactory != null ? httpClientFactory.CreateClient() : new HttpClient();
-
+    // according to the official website,
+    // we have to avoid using http client factory because we use cookies
+    var httpClient = new HttpClient();
+    
     try
     {
-        var voteInfoResponse = await httpClient.GetAsync($"https://localhost:7000/vote/{voteId}");
-
-        JsonNode? voteInfoBody = await JsonNode.ParseAsync(voteInfoResponse.Content.ReadAsStream());
-
-        if (!voteInfoResponse.IsSuccessStatusCode)
+        var credentialBody = JsonContent.Create(new
         {
-            Console.WriteLine($"Flood task {index} failed when fetching vote info");
-            if (voteInfoBody != null && voteInfoBody["message"] is JsonNode message)
+            Email = $"flooder{index}@gmail.com",
+            Password = "Password123@"
+        });
+
+        var loginResponse = await LoginAsync(httpClient, credentialBody);
+        if (loginResponse.cookie == null)
+        {
+            if (loginResponse.statusCode == HttpStatusCode.NotFound)
             {
-                Console.WriteLine($"Message: {message.GetValue<string>()}");
+                var registerRespone = await SendHttpRequestAsync(httpClient, RegisterUrl, HttpMethod.Post, credentialBody);
+                if (!registerRespone.success)
+                {
+                    Console.WriteLine($"Flood task {index} failed when registering: {registerRespone.message}");
+                    return;
+                }
+
+                loginResponse = await LoginAsync(httpClient, credentialBody);
             }
+        }
+
+        if (loginResponse.cookie == null)
+        {
+            Console.WriteLine($"Flood task {index} failed when logging in: {loginResponse.message}");
             return;
         }
 
-        if (voteInfoBody == null
-            || voteInfoBody["result"] is not JsonNode result)
+        var voteInfoResponse = await SendHttpRequestAsync(httpClient, $"{BaseVoteUrl}/{voteId}", HttpMethod.Get,
+            cookie: loginResponse.cookie);
+
+        if (!voteInfoResponse.success)
+        {
+            Console.WriteLine($"Flood task {index} failed when fetching vote info: {voteInfoResponse.message}");
+            return;
+        }
+
+        if (voteInfoResponse.resultJson is not JsonNode result)
         {
             return;
         }
@@ -162,18 +215,19 @@ static async Task FloodTaskAsync(int index, string voteId,
 
         bool isSuccessVote = false;
         int retryRemaining = voteMaxRetry + 1;
-
-        HttpResponseMessage? voteSubjectResponse = null;
-        JsonNode? voteSubjectBody = null;
+        string? message = null;
 
         while (!isSuccessVote && retryRemaining > 0)
         {
-            voteSubjectResponse = await httpClient.PostAsync($"https://localhost:7000/vote?voteId={voteId}&subjectId={subjectId}", null);
-            voteSubjectBody = await JsonNode.ParseAsync(voteSubjectResponse.Content.ReadAsStream());
+            var voteSubjectResponse = await SendHttpRequestAsync(httpClient,
+                $"{BaseVoteUrl}?voteId={voteId}&subjectId={subjectId}",
+                HttpMethod.Post,
+                cookie: loginResponse.cookie);
 
-            if (!voteSubjectResponse.IsSuccessStatusCode)
+            if (!voteSubjectResponse.success)
             {
                 retryRemaining--;
+                message = voteSubjectResponse.message;
             }
             else
             {
@@ -185,14 +239,7 @@ static async Task FloodTaskAsync(int index, string voteId,
 
         if (!isSuccessVote)
         {
-            var errorMessage = string.Empty;
-
-            if (voteSubjectBody != null && voteSubjectBody["message"] is JsonNode message)
-            {
-                errorMessage = $"Message: {message.GetValue<string>()}";
-            }
-
-            Console.WriteLine($"Flood task {index} failed when posting subject vote after retrying for {voteMaxRetry} time(s)\n{errorMessage}");
+            Console.WriteLine($"Flood task {index} failed when posting subject vote after retrying for {voteMaxRetry} time(s)\n{message}");
             return;
         }
 
@@ -212,7 +259,7 @@ static async Task FloodTaskAsync(int index, string voteId,
                 Console.WriteLine($"Json Error happened on flood task {index} while doing the request: {ex.Message}");
                 break;
             default:
-                Console.WriteLine($"Error happened on flood task {index} while doing the request: {ex.Message}");
+                Console.WriteLine($"Error happened ({ex.GetType()}) on flood task {index} while doing the request: {ex.Message}, {ex}");
                 break;
         }
 
@@ -229,14 +276,23 @@ static bool IsNumber(string input)
     return int.TryParse(input, out _);
 }
 
-static async Task<string?> PostVoteAsync(int? voteDuration = 60)
+static async Task<string?> PostVoteAsync(HttpClient? httpClient, Cookie? cookie, int? voteDuration = 60)
 {
     Console.WriteLine("Begin posting a vote");
 
     try
     {
-        var httpClient = new HttpClient();
-        var httpContent = JsonContent.Create(new
+        httpClient ??= new HttpClient();
+        
+        Console.WriteLine("Logging in...");
+        var loginResponse = await LoginCreatorAsync(httpClient);
+        if (loginResponse.cookie == null)
+        {
+            Console.WriteLine($"Failed to log in: {loginResponse.message}");
+            return null;
+        }
+
+        var votingContent = JsonContent.Create(new
         {
             Title = "Test vote",
             Subjects = new string[] { "Subject 1", "Subject 2" },
@@ -244,32 +300,26 @@ static async Task<string?> PostVoteAsync(int? voteDuration = 60)
         });
 
         Console.WriteLine("Posting a vote...");
-        var response = await httpClient.PostAsync("https://localhost:7000/vote/create", httpContent);
+        var response = await SendHttpRequestAsync(httpClient, CreateVoteUrl, HttpMethod.Post, votingContent, cookie);
+
+        if (!response.success)
+        {
+            Console.WriteLine($"Failed when posting vote: {response.message}");
+
+            return null;
+        }
+
+        if (response.resultJson == null)
+        {
+            return null;
+        }
+
+        if (response.resultJson["id"] is not JsonNode voteId)
+        {
+            return null;
+        }
+
         Console.WriteLine("Finished posting a vote");
-
-        var body = await JsonNode.ParseAsync(response.Content.ReadAsStream());
-
-        if (!response.IsSuccessStatusCode)
-        {
-            Console.WriteLine($"Posting vote response is not success: {response.StatusCode}");
-            if (body != null && body["message"] is JsonNode message)
-            {
-                Console.WriteLine($"Message: {message.GetValue<string>()}");
-            }
-
-            return null;
-        }
-
-        if (body == null
-            || body["result"] is not JsonNode result)
-        {
-            return null;
-        }
-
-        if (result["id"] is not JsonNode voteId)
-        {
-            return null;
-        }
 
         return voteId.GetValue<string>();
     }
@@ -292,5 +342,121 @@ static async Task<string?> PostVoteAsync(int? voteDuration = 60)
         }
 
         return null;
+    }
+}
+
+static Task<(Cookie? cookie, HttpStatusCode statusCode, string? message)> LoginCreatorAsync(HttpClient? httpClient)
+{
+    HttpContent creatorLoginRequestBody = JsonContent.Create(new
+    {
+        Email = Email,
+        Password = Password
+    });
+
+    return LoginAsync(httpClient, creatorLoginRequestBody);
+}
+
+static Cookie GetCookie(string setCookieString)
+{
+    ArgumentException.ThrowIfNullOrEmpty(setCookieString);
+
+    var splitted = setCookieString.Split("; ");
+    if (splitted.Length == 0)
+    {
+        return null!;
+    }
+
+    var cookie = new Cookie();
+    var nameValueSplit = splitted[0].Split("=");
+    if (nameValueSplit.Length == 2)
+    {
+        cookie.Name = nameValueSplit[0];
+        cookie.Value = nameValueSplit[1];
+    }
+
+    foreach (var piece in splitted)
+    {
+        if (piece.Contains("path"))
+        {
+            var splitPath = piece.Split("=");
+            if (splitPath.Length == 2)
+            {
+                cookie.Path = splitPath[1];
+            }
+        }
+        else if (piece.Contains("secure"))
+        {
+            cookie.Secure = true;
+        }
+        else if (piece.Contains("httponly"))
+        {
+            cookie.HttpOnly = true;
+        }
+    }
+
+    return cookie;
+}
+
+static async Task<(Cookie? cookie, HttpStatusCode statusCode, string? message)> LoginAsync(HttpClient? httpClient, HttpContent? requestBody)
+{
+    httpClient ??= new HttpClient();
+
+    var loginResponse = await SendHttpRequestAsync(httpClient, LoginUrl, HttpMethod.Post,
+        requestBody);
+
+    if (!loginResponse.success)
+    {
+        return (null, loginResponse.statusCode, loginResponse.message);
+    }
+
+    if (!loginResponse.headers.TryGetValues(ServerResponseHeaderCookieKey, out var cookies) || !cookies.Any())
+    {
+        return (null, loginResponse.statusCode, "Login success but no cookie returned");
+    }
+
+    return (GetCookie(cookies.Single()), loginResponse.statusCode, loginResponse.message);
+}
+
+static async Task<(bool success, HttpStatusCode statusCode, string? message, JsonNode? resultJson, HttpResponseHeaders headers)>
+    SendHttpRequestAsync(HttpClient httpClient, string url, HttpMethod method,
+        HttpContent? content = null, Cookie? cookie = null)
+{
+    var httpRequestMessage = new HttpRequestMessage()
+    {
+        RequestUri = new Uri(url),
+        Method = method,
+        Content = content,
+    };
+
+    if (cookie != null)
+    {
+        httpRequestMessage.Headers.Add("Cookie", cookie.ToString());
+    }
+
+    try
+    {
+        var response = await httpClient.SendAsync(httpRequestMessage);
+        var body = await JsonNode.ParseAsync(response.Content.ReadAsStream());
+        string? message = null;
+        JsonNode? resultObj = null;
+
+        if (body != null)
+        {
+            if (body["message"] is JsonNode messageJson)
+            {
+                message = messageJson.GetValue<string>();
+            }
+
+            if (body["result"] is JsonNode resultJson)
+            {
+                resultObj = resultJson;
+            }
+        }
+
+        return (response.IsSuccessStatusCode, response.StatusCode, message, resultObj, response.Headers);
+    }
+    catch (Exception)
+    {
+        throw;
     }
 }
