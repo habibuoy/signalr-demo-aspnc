51403682838980b10f9660adc6bd01cc22c26713
habibuoy <habibiemuhammad90@gmail.com>
2025-06-21 20:13:19 +0700

    Add loading in login page

--- sr-webclient/src/LoginPage.vue
+++ sr-webclient/src/LoginPage.vue
@@ -2,7 +2,7 @@
     <div class="flex flex-col items-center justify-center min-h-screen bg-gray-100">
         <div class="bg-white p-8 rounded shadow-md w-full max-w-sm">
             <h2 class="text-2xl font-bold mb-6 text-center">Login</h2>
-            <form @submit.prevent="onLogin">
+            <form @submit.prevent="onLoginClicked">
                 <div class="mb-4">
                     <label class="block mb-1 text-gray-700">Email</label>
                     <input v-model="email" type="email" class="w-full px-3 py-2 border rounded" required />
@@ -26,21 +26,33 @@
 import { ref } from 'vue'
 import { useRouter } from 'vue-router'
 import { login } from './access'
+import { spawnLoading } from './components/loading'
+import { spawnResultPopup } from './components/resultPopup'
 
 const email = ref('')
 const password = ref('')
 const router = useRouter()
 
-async function onLogin() {
-    console.log("Logging in")
-    const result = await login(email.value, password.value)
-    if (result) {
-        console.log(`Login successful`)
-        router.push('/')
-    }
-    else
-    {
-        console.log("Login failed")
+async function onLoginClicked() {
+    const loading = spawnLoading({loadingText: "Logging in..."}, 20)
+
+    try {
+        const result = await login(email.value, password.value)
+        const success = result && !result.errorMessage
+    
+        let feedbackText = "Successfully logged in"
+
+        if (!success) {
+            feedbackText = `Failed: ${result.errorMessage}`
+        }
+
+        const popup = spawnResultPopup({ feedbackText, success })
+        popup.onDestroy.subscribe(() => router.push('/'))
+    } catch (error) {
+        console.error(`Error happened while logging in ${email}`, error)
+        spawnResultPopup({ feedbackText: "Error logging in", success: false })
+    } finally {
+        loading.destroy()
     }
 }
 </script>
--- sr-webclient/src/access.js
+++ sr-webclient/src/access.js
@@ -1,48 +1,60 @@
-export { login, isAuthenticated, canManageVote }
+export { login, isAuthenticated, canManageVote, User }
+
+import { httpFetch, HttpMethod, BaseUrl } from './utils'
+
+const LoginPath = "/login"
+const LoginUrl = BaseUrl + LoginPath
+
+const ManageVotesAccessPath = "/votes/can-manage"
+const ManageVotesAccessUrl = BaseUrl + ManageVotesAccessPath
 
 async function canManageVote() {
-    const response = await fetch(`https://localhost:7000/votes/can-manage`, {
-        credentials: "include",
-    })
-
-    return response.ok
+    const result = await httpFetch(ManageVotesAccessUrl)
+    return result.isSuccess
 }
 
 async function login(email, password) {
-    const loginHeader = new Headers()
-    loginHeader.append("Content-Type", "application/json")
-    return await fetch("https://localhost:7000/login", {
-        method: "POST",
-        body: JSON.stringify({
-            email: email,
-            password: password,
-            remember: true,
-        }),
-        headers: loginHeader,
-        credentials: "include"
+    return httpFetch(LoginUrl, HttpMethod.POST, {
+        email: email,
+        password: password,
+        remember: true,
     })
-        .then(async response => Promise.resolve({ isSuccess: response.ok, json: await response.json() }))
         .then(response => {
+            const requestResult = {}
             if (!response.isSuccess) {
-                if (response.json.message) {
-                    console.log("Failed logging in: ", response.json.message)
-                }
-
-                return null
+                requestResult.errorMessage = response.message
+                requestResult.validationErrors = response.validationErrors
+            } else {
+                const user = response.result
+                requestResult.result = new User(
+                    user.id,
+                    user.email,
+                    user.firstName,
+                    user.lastName,
+                    user.createdTime
+                )
             }
 
-            if (!response.json.result) {
-                console.log("Login successful but no result")
-                return null
-            }
-
-            return response.json.result
+            return requestResult
         })
-        .catch(error => console.error("Error happeneed while logging in", error))
+        .catch(error => {
+            console.error(`Error happened while logging in user ${email}`, error)
+            return { errorMessage: "There was an error while logging in" }
+        })
 }
 
 // Check if a cookie named 'auth' exists
 function isAuthenticated() {
     const valid = document.cookie.split(';').some((c) => c.trim().startsWith('auth='))
     return valid
+}
+
+class User {
+    constructor(id, email, firstName, lastName, createdTime) {
+        this._id = id
+        this.email = email
+        this.firstName
+        this.lastName
+        this.createdTime
+    }
 }
