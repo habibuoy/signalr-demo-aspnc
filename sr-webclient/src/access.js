b4b96cf6edde33f5602acdb89e46442e0bdde0a0
habibuoy <habibiemuhammad90@gmail.com>
2025-06-22 09:06:18 +0700

    Move the register fetch method into access

--- sr-webclient/src/access.js
+++ sr-webclient/src/access.js
@@ -1,9 +1,13 @@
-export { login, isAuthenticated, canManageVote, User }
+export { login, register, isAuthenticated, canManageVote, User }
 
 import { httpFetch, HttpMethod, BaseUrl } from './utils'
 
 const LoginPath = "/login"
 const LoginUrl = BaseUrl + LoginPath
+
+const RegisterPath = "/register"
+const RegisterUrl = BaseUrl + RegisterPath
+
 
 const ManageVotesAccessPath = "/votes/can-manage"
 const ManageVotesAccessUrl = BaseUrl + ManageVotesAccessPath
@@ -40,6 +44,37 @@ async function login(email, password) {
         .catch(error => {
             console.error(`Error happened while logging in user ${email}`, error)
             return { errorMessage: "There was an error while logging in" }
+        })
+}
+
+async function register(email, password, firstName, lastName) {
+    return httpFetch(RegisterUrl, HttpMethod.POST, {
+            email: email,
+            password: password,
+            firstName: firstName,
+            lastName: lastName
+        })
+        .then(response => {
+            const requestResult = {}
+            if (!response.isSuccess) {
+                requestResult.errorMessage = response.message
+                requestResult.validationErrors = response.validationErrors
+            } else {
+                const user = response.result
+                requestResult.result = new User(
+                    user.id,
+                    user.email,
+                    user.firstName,
+                    user.lastName,
+                    user.createdTime
+                )
+            }
+
+            return requestResult
+        })
+        .catch(error => {
+            console.error(`Error happened while registering user ${email}`, error)
+            return { errorMessage: "There was an error while registering" }
         })
 }
 
